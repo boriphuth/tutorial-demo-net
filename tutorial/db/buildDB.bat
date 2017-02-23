25a55e08478d251d051f28e49030d8de2bac6540
boriphuth <boriphuth.saensukphattraka@live.com>
2017-02-23 16:47:47 +0700

    Iniitial commit

---
+++ tutorial/db/buildDB.bat
@@ -0,0 +1,7 @@
+@echo off
+SET DIR=%~d0%~p0%
+
+SET connstring="data source=WIN-MK4VOLD7ADV;initial catalog=Northwind;persist security info=True;user id=sa;password=Save7549;"
+SET repository.path="https://github.com/espiderinc/tutorial-demo-net.git"
+ 
+"%DIR%..\..\packages\roundhouse.0.8.6\bin\rh.exe" /c=%connstring% /vf=../bin/tutorial.dll /r=%repository.path% /env=LOCAL /ni /simple
