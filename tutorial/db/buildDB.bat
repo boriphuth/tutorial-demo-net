65f6f132e057bc2665077b10ec031372938398e6
boriphuth <boriphuth.saensukphattraka@live.com>
2017-02-24 10:46:15 +0700

    update git repository path

--- tutorial/db/buildDB.bat
+++ tutorial/db/buildDB.bat
@@ -2,6 +2,6 @@
 SET DIR=%~d0%~p0%
 
 SET connstring="data source=WIN-MK4VOLD7ADV\SQLEXPRESS;initial catalog=Northwind;persist security info=True;user id=sa;password=Save7549;"
-SET repository.path="https://github.com/espiderinc/tutorial-demo-net.git"
+SET repository.path="https://github.com/boriphuth/tutorial-demo-net.git"
  
 "%DIR%..\..\packages\roundhouse.0.8.6\bin\rh.exe" /c=%connstring% /vf=../bin/tutorial.dll /r=%repository.path% /env=LOCAL /ni /simple
